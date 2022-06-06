5d46b0ec2bf28aa581ec7b70c5ccf1bbf7f4b819
DrgnInventor <98103825+MuchMarts@users.noreply.github.com>
2022-06-06 22:11:54 +0300

    random = funny

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-function changeType(){
-    const elem = document.getElementsByClassName('banana');
-    console.log(elem)
-    if(elem[0].id == "0"){
-        elem[0].style.objectFit = "fill";
-        elem[0].id = "1";
-    }
-    else {
-        elem[0].style.objectFit = "contain";
-        elem[0].id = "0";
-    }
+var chance;
+
+function randomEqualFunny(){
+    chance = (Math.floor(Math.random()*100) + 1);
+    console.log(chance)
+    if(chance < 31)         {showYourself(1)}
+    else if(chance < 61)    {showYourself(2)}
+    else if(chance < 98)    {showYourself(3)}
+    else                    {showYourself(4)}
+}
+
+function showYourself(a){
+    document.getElementsByClassName("banana")[a-1].classList.toggle("hidden");
+}
+
+function changeType(a){
+    document.getElementById(a).classList.toggle("bigBoi");
 }
