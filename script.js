a37a0e47cfbd01c9e3bf534a07f3e875dd755246
DrgnInventor <98103825+MuchMarts@users.noreply.github.com>
2022-06-09 23:39:26 +0300

    Favicon update

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var chance;
 function randomEqualFunny(){
     chance = (Math.floor(Math.random()*100) + 1);
     console.log(chance)
-    if (chance == 69)       {showYourself(4)}
+    if (chance == 69)       {showYourself(4); changeFavicon("Icon.ico")}
     else if (chance < 33)   {showYourself(1)}
     else if (chance < 66)   {showYourself(2)} 
     else                    {showYourself(3)}
@@ -19,3 +19,17 @@ function changeType(a){
 
 const randomColor = "#"+((1<<24)*Math.random()|0).toString(16);
 document.documentElement.style.setProperty('--main-bg-color', randomColor);
+
+document.head = document.head || document.getElementsByTagName('head')[0];
+
+function changeFavicon(src) {
+ var link = document.createElement('link'),
+     oldLink = document.getElementById('dynamic-favicon');
+ link.id = 'dynamic-favicon';
+ link.rel = 'shortcut icon';
+ link.href = src;
+ if (oldLink) {
+  document.head.removeChild(oldLink);
+ }
+ document.head.appendChild(link);
+}
