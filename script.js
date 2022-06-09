1fd393aeca34b14b19457711a50aca4b25ac4e32
DrgnInventor <98103825+MuchMarts@users.noreply.github.com>
2022-06-09 19:01:59 +0300

    696969

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ var chance;
 function randomEqualFunny(){
     chance = (Math.floor(Math.random()*100) + 1);
     console.log(chance)
-    if(chance < 31)         {showYourself(1)}
-    else if(chance < 61)    {showYourself(2)}
-    else if(chance < 98)    {showYourself(3)}
-    else                    {showYourself(4)}
+    if (chance == 69)       {showYourself(4)}
+    else if (chance < 33)   {showYourself(1)}
+    else if (chance < 66)   {showYourself(2)} 
+    else                    {showYourself(3)}
 }
 
 function showYourself(a){
-    document.getElementsByClassName("banana")[a-1].classList.toggle("hidden");
+    document.getElementsByClassName("banana")[a-1].classList.toggle("shown");
 }
 
 function changeType(a){
     document.getElementById(a).classList.toggle("bigBoi");
 }
 
-const randomColor = "#"+((1<<24)*Math.random()|0).toString(16); 
+const randomColor = "#"+((1<<24)*Math.random()|0).toString(16);
 document.documentElement.style.setProperty('--main-bg-color', randomColor);
