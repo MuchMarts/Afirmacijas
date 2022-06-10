5ae4d3d2ac6fd230d8ae82d2b66c70c5a0d85ce3
DrgnInventor <98103825+MuchMarts@users.noreply.github.com>
2022-06-10 22:58:36 +0300

    diffq

--- script.js
+++ script.js
@@ -1,9 +1,51 @@
+const reslq = 100;
+const resmq = 1000;
+const reshq = 4200;
 
 var canvas = document.getElementById('viewport');
 context = canvas.getContext('2d');
 
 console.log(canvas);
 
+class afirmation {
+    constructor(){
+        this.imgUrl;
+        this.text;
+        this.textCol;
+        this.borderCol;
+    };
+    setUrl(url) {
+        this.imgUrl = url;
+    }
+
+    createCanvas(size){
+        var new_canvas = document.createElement('canvas');
+        new_canvas.id = "finalCopy";
+        new_canvas.width = size;
+        new_canvas.height = size;
+        var body = document.getElementById('finished');
+        body.appendChild(new_canvas);
+    }
+
+    saveCanva(res){
+        this.createCanvas(res);
+        var bob = document.getElementById("finalCopy");
+        add_img(this.imgUrl);
+        save_canvas(bob);
+        document.getElementById('finished').removeChild(bob);
+    }
+    saveHQ(){
+        this.saveCanva(reshq);
+    }
+    saveMQ(){
+        this.saveCanva(resmq);
+    }
+    saveLQ(){
+        this.saveCanva(reslq);
+    }
+}
+
+let igors = new afirmation(); 
 
 function add_img(src) {
     img = new Image();
@@ -13,11 +55,22 @@ function add_img(src) {
     context.drawImage(img,0,0,1000,1000);
     }
 }
-function save_canvas() {
+function save_canvas(canva) {
     var link = document.createElement('a');
     link.download = 'afirmacija.png';
-    link.href = canvas.toDataURL();
+    link.href = canva.toDataURL();
     link.click();
+}
+
+function hq(){
+    igors.saveHQ;
+}
+function mq(){
+    igors.saveMQ;
+}
+
+function lq(){
+    igors.saveLQ;
 }
 
 function showYourself(a){
@@ -37,6 +90,8 @@ function handleFiles(){
     } else if(userFile.files.length > 1){
         place.innerHTML = "Izvēlies vienu failu pls...:(";   
     } else {
-       add_img(URL.createObjectURL(userFile.files[0]));
+        var tempURL = URL.createObjectURL(userFile.files[0]);
+        igors.setUrl(tempURL);
+        add_img(tempURL);
     }
 }
