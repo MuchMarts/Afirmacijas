55489b85d0f4a78f6cf3da2acd8236e3a0f1b0c0
DrgnInventor <98103825+MuchMarts@users.noreply.github.com>
2022-06-10 19:22:20 +0300

    img background

--- script.js
+++ script.js
@@ -1,35 +1,15 @@
-var chance;
+var canvas = document.getElementById('viewport');
+context = canvas.getContext('2d');
 
-function randomEqualFunny(){
-    chance = (Math.floor(Math.random()*100) + 1);
-    console.log(chance)
-    if (chance == 69)       {showYourself(4); changeFavicon("Icon.ico")}
-    else if (chance < 33)   {showYourself(1)}
-    else if (chance < 66)   {showYourself(2)} 
-    else                    {showYourself(3)}
+console.log(canvas);
+
+
+function add_img(src) {
+    img = new Image();
+    img.src = src;
+    img.onload = function(){
+        context.drawImage(img,0,0,1000,1000)
+    }
 }
 
-function showYourself(a){
-    document.getElementsByClassName("banana")[a-1].classList.toggle("shown");
-}
-
-function changeType(a){
-    document.getElementById(a).classList.toggle("bigBoi");
-}
-
-const randomColor = "#"+((1<<24)*Math.random()|0).toString(16);
-document.documentElement.style.setProperty('--main-bg-color', randomColor);
-
-document.head = document.head || document.getElementsByTagName('head')[0];
-
-function changeFavicon(src) {
- var link = document.createElement('link'),
-     oldLink = document.getElementById('dynamic-favicon');
- link.id = 'dynamic-favicon';
- link.rel = 'shortcut icon';
- link.href = src;
- if (oldLink) {
-  document.head.removeChild(oldLink);
- }
- document.head.appendChild(link);
-}
+add_img('a1.png')
