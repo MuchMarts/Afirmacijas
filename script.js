6c8fce8f581b963b594df1d1a619fc1fed71dea4
DrgnInventor <98103825+MuchMarts@users.noreply.github.com>
2022-06-14 02:29:28 +0300

    text, clear button and more usage of stor

--- script.js
+++ script.js
@@ -9,13 +9,16 @@ var canw = 0; var canh = 0;
 function ConstAfirmacija() {
     
     this.imgURL = "notSet";
-    this.text = "notSet";
-    this.textCol = "notSet";
+    this.text = {
+        topTxt : "",
+        botTxt : "",
+        txtSize: ""
+    }
     this.borderCol = "notSet";
 }  
 const igors = new ConstAfirmacija();
 
-const canvas = document.getElementById("viewport");
+const canvas = document.getElementById("image");
 context = canvas.getContext('2d');
 
 //Diferent workspace size for diferent device
@@ -38,27 +41,43 @@ function add_img(src) {
 }
 
 //Merge multiple canvas
-function mergeCanvas(imageID, borderID, textID){
+function mergeCanvas(imageID, borderID, textID, textBlurID, ctx, size){
     var baseCanva = document.getElementById(imageID);
     var border = document.getElementById(borderID);
-
-    var ctx = baseCanva.getContext("2d");
-    ctx.drawImage(border, 0, 0, canw, canh);
+    var text = document.getElementById(textID);
+    var textBlur = document.getElementById(textBlurID);
+    
+    ctx.drawImage(baseCanva, 0, 0, size, size);
+    ctx.drawImage(border, 0, 0, size, size);
+    ctx.drawImage(textBlur, 0, 0, size, size);
+    ctx.drawImage(text, 0, 0, size, size);
 }
 
 //Show/Hide smth
-function showYourself(a){document.getElementsByClassName("banana")[a-1].classList.toggle("hidden")}
+function showYourself(a){document.getElementsByClassName("banana")[a-1].classList.toggle("hidden");}
 
 //Strecth smth
-function changeType(a){document.getElementById(a).classList.toggle("bigBoi")}
+function changeType(a){document.getElementById(a).classList.toggle("bigBoi");}
 
 //Used to save image
 function saveFile(exportSize){downloadCanvas(exportSize);}
 
+//Clear canvas
+function clearCanvas(elem){
+    var ctx = elem.getContext("2d");
+    ctx.clearRect(0, 0, canh, canw);}
+
+//Clear all
+function clearAll(){
+    clearCanvas(document.getElementById("image"));
+    clearCanvas(document.getElementById("border"));
+    clearCanvas(document.getElementById("text"));
+    clearCanvas(document.getElementById("textBlur"));
+}
 // Creates final canva and exports it
 function downloadCanvas(size){
     //Merge all layers
-    mergeCanvas("viewport", "border");
+    
     
     //Create final canva element
     var new_canvas = document.createElement("canvas");
@@ -67,7 +86,7 @@ function downloadCanvas(size){
     new_canvas.height = size;
 
     let nwctx = new_canvas.getContext("2d");
-    nwctx.drawImage(canvas, 0, 0, size, size);
+    mergeCanvas("image", "border", "text", "textBlur", nwctx, size);
 
     let canvasUrl = new_canvas.toDataURL();
     
@@ -116,35 +135,37 @@ function convertHex(hexCode, opacity = 1){ //stolen code src="https://gist.githu
     return rgba;
 }
 
-const brdwd = canw/30;
 
-var borderlayer = document.getElementById("border");
+const borderlayer = document.getElementById("border");
 
 borderlayer.setAttribute("width", canw);
 borderlayer.setAttribute("height", canh);
 
-var ctxb = borderlayer.getContext("2d");
+const ctxb = borderlayer.getContext("2d");
 
 var color = document.getElementById("colorpicker").value;
 
 document.getElementById("colorpicker").addEventListener("change", function(){
     color = this.value;
     igors.borderCol = color;
-    setBorder();
+    setBorder(color, ctxb, canw);
 });
 
-function setBorder(){  
-    ctxb.fillStyle = setGradient(0, 0, 0, brdwd, color);
-    ctxb.fillRect(0, 0, canw, brdwd); //top
+function setBorder(borderColour, ctxborder, width){  
 
-    ctxb.fillStyle = setGradient(0, 0, brdwd, 0, color);
-    ctxb.fillRect(0, 0, brdwd, canh); //left
+    borderWidth = width/30;
 
-    ctxb.fillStyle = setGradient(0, canh, 0, canh-brdwd, color);
-    ctxb.fillRect(0, canh-brdwd, canw, brdwd); //bottom
+    ctxborder.fillStyle = setGradient(0, 0, 0, borderWidth, borderColour);
+    ctxborder.fillRect(0, 0, width, borderWidth); //top
 
-    ctxb.fillStyle = setGradient(canw, 0, canw-brdwd, 0, color);
-    ctxb.fillRect(canw-brdwd, 0, brdwd, canh); //right
+    ctxborder.fillStyle = setGradient(0, 0, borderWidth, 0, borderColour);
+    ctxborder.fillRect(0, 0, borderWidth, width); //left
+
+    ctxborder.fillStyle = setGradient(0, width, 0, width-borderWidth, borderColour);
+    ctxborder.fillRect(0, width-borderWidth, width, borderWidth); //bottom
+
+    ctxborder.fillStyle = setGradient(width, 0, width-borderWidth, 0, borderColour);
+    ctxborder.fillRect(width-borderWidth, 0, borderWidth, width); //right
 }
 
 function setGradient(x, y, x1, y1, color){
@@ -157,3 +178,42 @@ function setGradient(x, y, x1, y1, color){
     gradient.addColorStop(1, `rgba(${r},${g},${b},0)`);
     return gradient;
 }
+
+//Text bs
+
+const maxWidth = canw * 0.9;
+
+const textLayer = document.getElementById("text");
+
+textLayer.setAttribute("width", canw);
+textLayer.setAttribute("height", canh);
+
+const txtctx = textLayer.getContext("2d");
+
+const textBlurLayer = document.getElementById("textBlur");
+
+textBlurLayer.setAttribute("width", canw);
+textBlurLayer.setAttribute("height", canh);
+
+const blurctx = textBlurLayer.getContext("2d");
+
+function drawText(text, fontSize, x, y, txtCon, blurCon){
+    txtCon.textAlign = 'center';    
+    txtCon.font = 'bold '+fontSize + 'px serif';
+    txtCon.fillStyle = '#FFFFFF';
+    txtCon.fillText(text, x, y, maxWidth);
+    
+    blurCon.filter = 'blur(5px)';
+    blurCon.textAlign = 'center';    
+    blurCon.font = 'bold '+fontSize + 'px serif';
+    blurCon.fillStyle = '#00FF00';
+    blurCon.fillText(text, x, y, maxWidth);
+
+}
+
+igors.text.topTxt = "Es esmu laba";
+igors.text.botTxt = "SKOLOTAJA"
+igors.text.size = 74;
+
+drawText("Es esmu laba", 74, canw/2, canh*0.1, txtctx, blurctx);
+drawText("SKOLOTAJA", 74, canw/2, canh*0.9, txtctx, blurctx);
