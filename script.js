620f953e76be904a0c736d08e0c7508af8b5da64
gdarta <dartagobleja@gmail.com>
2022-06-13 22:41:15 +0300

    border with rect gradient
    -need to figure out filters on safari for max pretty borders

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const resmq = 720;
 const reshq = 1080;
 
 var canvas = document.getElementById("viewport");
-context = canvas.getContext('2d');
+var context = canvas.getContext('2d');
 
 function ConstAfirmacija() {
     this.imgURL = "notSet";
@@ -24,8 +24,39 @@ if(width > 1050 && height > 1050){
 }
 canvas.setAttribute("width", canw);
 canvas.setAttribute("height", canh);
+
 const igors = new ConstAfirmacija();
-    
+
+function mergeCanvas(pictureID, overlayID){
+    var picture = document.getElementById(pictureID);
+    var overlay = document.getElementById(overlayID);
+
+    var pctx = picture.getContext("2d");
+    pctx.drawImage(overlay, 0, 0, canw, canh);
+}
+
+function downloadCanvas(size){
+    mergeCanvas("viewport", "border");
+    var new_canvas = document.createElement("canvas");
+    new_canvas.id = "finalCopy";
+    new_canvas.width = size;
+    new_canvas.height = size;
+    var body = document.getElementById('finished');
+    //body.appendChild(new_canvas);
+
+    let nwctx = new_canvas.getContext("2d");
+    nwctx.drawImage(canvas, 0, 0, size, size);
+
+    let canvasUrl = new_canvas.toDataURL();
+    const temp = document.createElement("a");
+    temp.href = canvasUrl;
+    temp.download = "afirmacija";
+    temp.click();
+    temp.remove;
+
+}
+
+/*    
 function createCanvas(size){
     var new_canvas = document.createElement('canvas');
     new_canvas.id = "finalCopy";
@@ -41,35 +72,37 @@ function dwnImg(size){
     bob.classList.add('sneaky')
     f_add_img(igors.imgURL, size, bob);
 }
-
+*/
 function saveHQ(){
-    dwnImg(reshq);
+    downloadCanvas(reshq);
 }
+
 function saveMQ(){
-    dwnImg(resmq);
+    downloadCanvas(resmq);
 }
 function saveLQ(){
-    dwnImg(reslq);
+    downloadCanvas(reslq);
 }
-
-function f_add_img(src, size, bob) {
+/*
+function f_add_img(canvas, size, bob) {
     img = new Image();
     new_context = bob.getContext('2d');
     img.src = src;
-    img.onload = function(){
-    new_context.drawImage(img,0,0,size,size);
-    save_canvas(bob);
-    document.getElementById('finished').removeChild(bob);
-    }
+    //img.onload = function(){
+        new_context.drawImage(img,0,0,size,size);
+        save_canvas(bob);
+        document.getElementById('finished').removeChild(bob);
+    //}
 }
-
+*/
 function add_img(src) {
     img = new Image();
     img.src = src;
     img.onload = function(){
-    context.drawImage(img,0,0,canw,canh);
+        context.drawImage(img,0,0,canw,canh);
     }
 }
+/*
 function save_canvas(canva) {
     var link = document.createElement('a');
     link.download = 'afirmacija.png';
@@ -78,7 +111,7 @@ function save_canvas(canva) {
     link.click();
     document.body.removeChild(link);
 }
-
+*/
 function test_save(){
 }
 
@@ -117,4 +150,60 @@ function handleFiles(){
         igors.imgURL = tempUrl;
         add_img(tempUrl);
     }
+}
+
+function convertHex(hexCode, opacity = 1){ //stolen code src="https://gist.github.com/danieliser/b4b24c9f772066bcf0a6.js"
+    var hex = hexCode.replace('#', '');
+
+    if (hex.length === 3) {
+        hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+    }
+
+    var r = parseInt(hex.substring(0,2), 16),
+        g = parseInt(hex.substring(2,4), 16),
+        b = parseInt(hex.substring(4,6), 16);
+
+    /* Backward compatibility for whole number based opacity values. */
+    if (opacity > 1 && opacity <= 100) {
+        opacity = opacity / 100;   
+    }
+    var rgba = [r, g, b, opacity];
+    return rgba;
+}
+
+const brdwd = canw/30;
+var borderlayer = document.getElementById("border");
+borderlayer.setAttribute("width", canw);
+borderlayer.setAttribute("height", canh);
+var ctxb = borderlayer.getContext("2d");
+var color = document.getElementById("colorpicker").value;
+document.getElementById("colorpicker").addEventListener("change", function(){
+    color = this.value;
+    igors.borderCol = color;
+    setBorder();
+});
+
+function setBorder(){  
+    ctxb.fillStyle = setGradient(0, 0, 0, brdwd, color);
+    ctxb.fillRect(0, 0, canw, brdwd); //top
+
+    ctxb.fillStyle = setGradient(0, 0, brdwd, 0, color);
+    ctxb.fillRect(0, 0, brdwd, canh); //left
+
+    ctxb.fillStyle = setGradient(0, canh, 0, canh-brdwd, color);
+    ctxb.fillRect(0, canh-brdwd, canw, brdwd); //bottom
+
+    ctxb.fillStyle = setGradient(canw, 0, canw-brdwd, 0, color);
+    ctxb.fillRect(canw-brdwd, 0, brdwd, canh); //right
+}
+
+function setGradient(x, y, x1, y1, color){
+    var rgba = convertHex(color, 1);
+    var r = rgba[0], g = rgba[1], b = rgba[2];
+    var gradient = ctxb.createLinearGradient(x, y, x1, y1);
+    gradient.addColorStop(0, `rgba(${r},${g},${b},1)`);
+    //gradient.addColorStop(0.35, `rgba(${r},${g},${b},.95)`);
+    gradient.addColorStop(0.7, `rgba(${r},${g},${b},.2)`);
+    gradient.addColorStop(1, `rgba(${r},${g},${b},0)`);
+    return gradient;
 }
