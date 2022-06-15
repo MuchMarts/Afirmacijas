b7db3bfcdb9bcd3b1d500517842e1ee6e9e23b56
DrgnInventor <98103825+MuchMarts@users.noreply.github.com>
2022-06-15 06:37:45 +0300

    change font size

--- script.js
+++ script.js
@@ -1,32 +1,11 @@
+window.onload = init;
+
 //  Properties
 const width = window.innerWidth;
 const height = window.innerHeight;
 
 var canw = 0; var canh = 0;
 
-//Storage
-function ConstAfirmacija() {
-    
-    this.imgURL = "null";
-    this.text = {
-        topTxt : "null",
-        botTxt : "null",
-        txtRatio: -1
-    }
-    this.borderCol = "null";
-}  
-const igors = new ConstAfirmacija();
-
-function init(){
-    document.getElementById("toptext").value = "";
-    document.getElementById("bottomtext").value = "";
-
-}
-window.onload = init;
-
-const canvas = document.getElementById("image");
-const context = canvas.getContext('2d');
-   
 //Diferent workspace size for diferent device
 if(width>1050 && height>1050){canw = 1000; canh = 1000;
 } else {
@@ -34,12 +13,31 @@ if(width>1050 && height>1050){canw = 1000; canh = 1000;
     if(width<height){canw = width - 50; canh = width - 50;}
 }
 
+var imgURL = "";
+var text = {  
+    topTxt : "null",
+    botTxt : "null",
+    txtRatio: 100/canw
+}
+var borderCol = "null";
+
+const canvas = document.getElementById("image");
+const context = canvas.getContext('2d');
+   
+
+
 canvas.setAttribute("width", canw);
 canvas.setAttribute("height", canh);
 
 document.getElementById('result').style.width = canw + 'px';
 document.getElementById('result').style.height = canh + 'px';
 document.getElementById('interface').style.width = canw + 'px';
+
+function init(){
+    document.getElementById("toptext").value = "";
+    document.getElementById("bottomtext").value = "";
+    document.getElementById("myRange").value = 100;
+    }
 
 // Add image on canva
 function add_img(src, canva, size) {
@@ -70,19 +68,17 @@ function constructCanva(canva, size){
 
     var ctx = canva.getContext("2d");
     img = new Image();
-    img.src = igors.imgURL;
+    img.src = imgURL;
     img.onload = function(){
         ctx.drawImage(img,0,0,size,size);
-        if(igors.borderCol != "null"){setBorder(igors.borderCol, ctx, size);}
-        if(igors.text.topTxt != "null" && igors.text.txtRatio != -1){
-            drawTopTxt(canva, igors.text.topTxt, igors.text.txtRatio);
+        if(borderCol != "null"){setBorder(borderCol, ctx, size);}
+        if(text.topTxt != "null" && text.txtRatio != -1){
+            drawTopTxt(canva, text.topTxt, text.txtRatio);
         }
-        if(igors.text.botTxt != "null" && igors.text.txtRatio != -1){
-            drawBotTxt(canva, igors.text.botTxt, igors.text.txtRatio);
+        if(text.botTxt != "null" && text.txtRatio != -1){
+            drawBotTxt(canva, text.botTxt, text.txtRatio);
         }
         
-        //ctx.drawImage(document.getElementById("text"), 0, 0, size, size)
-
         const temp = document.createElement("a");
         temp.href = canva.toDataURL();
         temp.download = "afirmacija";
@@ -112,13 +108,10 @@ function clearAll(){
     clearCanvas(document.getElementById("image"));
     clearCanvas(document.getElementById("border"));
     clearCanvas(document.getElementById("text"));
-    clearCanvas(document.getElementById("textBlur"));
 }
 // Creates final canva and exports it
 function downloadCanvas(size){
-    //Merge all layers
-    
-    
+        
     //Create final canva element
     var anch = document.getElementById("finished");
     var new_canvas = document.createElement("canvas");
@@ -145,7 +138,7 @@ function handleFiles(){
     } else {
         place.innerHTML = userFile.files[0].name + ": " + userFile.files[0].size + " biti";
         tempUrl = URL.createObjectURL(userFile.files[0]);
-        igors.imgURL = tempUrl;
+        imgURL = tempUrl;
         add_img(tempUrl, canvas, canw);
     }
 }
@@ -169,7 +162,6 @@ function convertHex(hexCode, opacity = 1){ //stolen code src="https://gist.githu
     return rgba;
 }
 
-
 const borderlayer = document.getElementById("border");
 
 borderlayer.setAttribute("width", canw);
@@ -181,7 +173,7 @@ var color = document.getElementById("colorpicker").value;
 
 document.getElementById("colorpicker").addEventListener("change", function(){
     color = this.value;
-    igors.borderCol = color;
+    borderCol = color;
     setBorder(color, ctxb, canw);
 });
 
@@ -236,13 +228,11 @@ function drawText(text, fontSize, x, y, txtCon, size){
     txtCon.fillText(text, x, y, maxWidth);
 }
 
-igors.text.txtRatio = 100/canw;
-
 document.getElementById("textcolor").addEventListener("change", function(){
-    if(igors.text.botTxt.length != 0){
+    if(text.botTxt.length != 0){
         setBttmTxt();
     }
-    if(igors.text.botTxt.length != 0){
+    if(text.botTxt.length != 0){
         setTopTxt();
     }
 })
@@ -260,15 +250,15 @@ function drawBotTxt(canva, text, txtRatio){
 function setBttmTxt(){
     txtctx.clearRect(0, 0, canw, canh/2);
     //blurctx.clearRect(0, 0, canw, canh/2);
-    igors.text.topTxt = document.getElementById("toptext").value.toUpperCase();
-    drawText(igors.text.topTxt, igors.text.txtRatio, canw/2, canh*0.15, txtctx, canw);
+    text.topTxt = document.getElementById("toptext").value.toUpperCase();
+    drawText(text.topTxt, text.txtRatio, canw/2, canh*0.15, txtctx, canw);
 }
 
 function setTopTxt(){
     txtctx.clearRect(0, canh/2, canw, canh/2);
     //blurctx.clearRect(0, canh/2, canw, canh/2);
-    igors.text.botTxt = document.getElementById("bottomtext").value.toUpperCase();
-    drawText(igors.text.botTxt, igors.text.txtRatio, canw/2, canh*0.9, txtctx, canw);
+    text.botTxt = document.getElementById("bottomtext").value.toUpperCase();
+    drawText(text.botTxt, text.txtRatio, canw/2, canh*0.9, txtctx, canw);
 }
 
 document.getElementById("toptext").addEventListener("change", function(){
@@ -278,3 +268,11 @@ document.getElementById("toptext").addEventListener("change", function(){
 document.getElementById("bottomtext").addEventListener("change", function(){
     document.fonts.load("Work Sans").then(setTopTxt());
 });
+
+var slider = document.getElementById("myRange");
+
+slider.oninput = function() {
+    text.txtRatio = this.value/canw;
+    document.fonts.load("Work Sans").then(setBttmTxt());
+    document.fonts.load("Work Sans").then(setTopTxt());
+}
