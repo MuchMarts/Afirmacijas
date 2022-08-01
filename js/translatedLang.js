ffa45975edacce73192757d26db62133d0482edd
DrgnInventor <98103825+MuchMarts@users.noreply.github.com>
2022-08-01 19:59:01 +0300

    using lingumania

---
+++ js/translatedLang.js
@@ -0,0 +1,19 @@
+var linguJSON = {
+    "languages": [
+        {
+            "lang_name": "Latvian",
+            "lang_code": "lv",
+            "url_pattern": ""
+        },
+        {
+            "lang_name": "English",
+            "lang_code": "en",
+            "url_pattern": ""
+        }
+    ],
+    "translated_segments": [
+        {
+        },
+
+    ]
+};
